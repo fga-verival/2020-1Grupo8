abd2f5143daa6c47b327efc17b8a3c7bb9d02b45
damarcones <damarcones@gmail.com>
2020-11-01 18:27:14 -0300

    Obtendo  barra vermelha por novo teste para a mesma unidade previamente implementado Signed-off-by: damarcones <damarcones@gmail.com> > Co-authored-by: brunaalmeidasantos <brunaalmeida48@gmail.com>

--- calculadora/src/tst/TesteImpostoRenda.java
+++ calculadora/src/tst/TesteImpostoRenda.java
@@ -14,5 +14,10 @@ public class TesteImpostoRenda {
 		Calculadora imposto  = new Calculadora(240,3000.00f,9.0f,20.0f);
 		assertEquals(35.51f, imposto.getImpostoRenda(),0.1f);
 	}
-
+	
+	@Test
+	public void testeImpostoRenda2(){
+		Calculadora imposto = new Calculadora(550,400.00f,8.0f,17.5f); 
+		assertEquals(8.44f, imposto.getImpostoRenda(),0.1f);
+	}
 }
