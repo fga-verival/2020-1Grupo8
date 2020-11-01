9e1d93ea93f794a3733f1593f2bb21363b1e0ee0
Luciano Santos <luciano_z7@hotmail.com>
2020-11-01 11:44:54 -0300

    Obtendo barra verde apos oferecer implementacao para unidade sob desenvolvimento do rendimento bruto
    Co-authored-by: gleal17 <guilhermelml@gmail.com>
    Co-authored-by: FranciscoHeronildo <heronildo.2010@hotmail.com>

--- calculadora/src/tst/TesteRendimentoBruto.java
+++ calculadora/src/tst/TesteRendimentoBruto.java
@@ -12,7 +12,7 @@ public class TesteRendimentoBruto {
 	@Test
 	public void testeRendimentoBruto() {
 		Calculadora calculadora = new Calculadora(240,3000.00f,9.0f,20.0f);
-		assertEquals(14.97f, calculadora.getRendimentoBruto(), 0.01f);
+		assertEquals(177.53f, calculadora.getRendimentoBruto(), 0.01f);
 	}
 
 }
