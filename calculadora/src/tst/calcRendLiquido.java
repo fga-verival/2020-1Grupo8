38e7a9f315bfd21db63a2187f93d352ed148c66e
KalebeLopes <calbkalebe@gmail.com>
2020-11-02 04:07:36 -0300

    obter barra vermelha por falta de implementação da unidade de teste do rendimento liquido
    Co-authored-by: muriloschiler <muriloschiler@gmail.com>
    Co-authored-by: joaoCeilandia <joaovitordmrosa@hotmail.com>

---
+++ calculadora/src/tst/calcRendLiquido.java
@@ -0,0 +1,14 @@
+package tst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class calcRendLiquido {
+
+	@Test
+	public void rendimentoLiquido() {
+		fail("Not yet implemented");
+	}
+
+}
